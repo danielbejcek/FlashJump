73b15b03b1001e7849f659f1165f889a5455770e
danielbejcek <daniel.bejcek@seznam.cz>
2023-12-14 11:19:27 +0100

    Updated X and Y movement axis.

--- Core/main.py
+++ Core/main.py
@@ -2,20 +2,79 @@ import sys
 import pygame
 
 
+class Game:
+    def __init__(self):
+        self.img_paths = {
+            "BG_image": "D:\Documents\PyCharm projects\PyCharm projects\PyGame\FlashJump\Images\BG_MAIN.png",
+            "particle_img1": "D:\Documents\PyCharm projects\PyCharm projects\PyGame\FlashJump\Images\Particles\leaf_particle_1.png",
+            "character_img": "D:\Documents\PyCharm projects\PyCharm projects\PyGame\FlashJump\Images\Character\character.png",
 
-def _main():
-    pygame.init()
-    pygame.display.set_mode((1792, 1024))
-    pygame.display.set_caption("FlashJump")
-    clock = pygame.time.Clock()
+        }
+        pygame.init()
+        self.screen = pygame.display.set_mode((1792, 1024))
+        pygame.display.set_caption("FlashJump")
+        self.clock = pygame.time.Clock()
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        pygame.display.update()
-        clock.tick(60)
+        self.bg_img = pygame.image.load(self.img_paths["BG_image"])
+        self.character_img = pygame.image.load(self.img_paths["character_img"])
+
+        self.movement_y = [False, False]
+        self.movement_x = [False, False]
+        self.img_pos = [160,260]
+        self.collision_area = pygame.Rect(200,100,100,100)
+
+    def run(self):
+
+        while True:
+            self.screen.blit(self.bg_img, (0,0))
+
+            self.img_pos[1] += (self.movement_y[1] - self.movement_y[0]) * 5
+            self.img_pos[0] += (self.movement_x[1] - self.movement_x[0]) * 5
+
+
+            img_rect = pygame.Rect(self.img_pos[0], self.img_pos[1], self.character_img.get_width(), self.character_img.get_height())
+
+
+            if img_rect.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen,(0,10,255),self.collision_area)
+            else:
+                pygame.draw.rect(self.screen,(0,50,155),self.collision_area)
+
+            self.screen.blit(self.character_img, self.img_pos)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    """Y Axis"""
+                    if event.key == pygame.K_w:
+                        self.movement_y[0] = True
+                    if event.key == pygame.K_s:
+                        self.movement_y[1] = True
+                    """X Axis"""
+                    if event.key == pygame.K_a:
+
+                        self.movement_x[0] = True
+                    if event.key == pygame.K_d:
+                        self.movement_x[1] = True
+
+
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        self.movement_y[0] = False
+                    if event.key == pygame.K_s:
+                        self.movement_y[1] = False
+                    if event.key == pygame.K_a:
+                        self.movement_x[0] = False
+                    if event.key == pygame.K_d:
+                        self.movement_x[1] = False
+
+
+            pygame.display.update()
+            self.clock.tick(60)
+
 
 if __name__ == "__main__":
-    _main()
+    Game().run()
--- Tests/test_main.py
+++ Tests/test_main.py
@@ -1,18 +1,22 @@
+from Core import main
 import unittest
 from unittest.mock import Mock
 from unittest.mock import patch, MagicMock
 import pygame
 import sys
-from FlashJump.Core import main
 
 
 class TestGameWindow(unittest.TestCase):
+
+    def setUp(self):
+        self.ref_images = main.Game().img_paths
+        self.test_bg_img = self.ref_images["BG_image"]
+
+    @patch('pygame.image.load')
     @patch('pygame.display.set_caption')
     @patch('pygame.display.set_mode')
     @patch('pygame.init')
-
-    def test_window_opening(self, mock_pygame_init, mock_set_mode, mock_set_caption):
-
+    def test_window_opening(self, mock_pygame_init, mock_set_mode, mock_set_caption, mock_load):
         """
         'mock_event' is a mock object to simulate a Pygame event. 
         In the context of this test, we're specifically creating a mock event that simulates the pygame.QUIT event. 
@@ -26,13 +30,20 @@ class TestGameWindow(unittest.TestCase):
         mock_event.type = pygame.QUIT
         with patch('pygame.event.get', return_value=[mock_event]):
             try:
-                main._main()
+                main.Game()
+                print("Window has been innitialized successfuly.")
             except SystemExit:
-                pass
+               pass
+
 
         # Assert that pygame.init() was called
         mock_pygame_init.assert_called_once()
-
         mock_set_mode.assert_called_once_with((1792, 1024))
         mock_set_caption.assert_called_once_with("FlashJump")
+
+        mock_load.assert_any_call(self.test_bg_img)
+        self.assertEqual(self.test_bg_img,"D:\Documents\PyCharm projects\PyCharm projects\PyGame\FlashJump\Images\BG_MAIN.png")
+
+
+
 
