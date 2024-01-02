cf831d529057035a0041a790d83f0a33822e44a0
danielbejcek <daniel.bejcek@seznam.cz>
2024-01-02 12:44:51 +0100

    Overlapping keys issue resolved to match fluence of the character movement.

--- Main/player.py
+++ Main/player.py
@@ -10,20 +10,24 @@ class PlayerCharacter(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.action = 'Idle'
 
-
         """Serves as a divider between actions. 0 is for idle, 1 is for running"""
         self.action_divider = 0
 
+        """'self.frame_index' serves to access the specific image as an index in a 'animation_list' received from 'animate_character' function."""
         self.frame_index = 0
         self.update_time = pygame.time.get_ticks()
-
-        # self.image = animate_character(self.action)[self.action_divider][self.frame_index]
 
         self.movement_y = [False, False]
         self.movement_x = [False, False]
         self.img_pos = [x, y]
         self.flip = False
 
+        """
+        Variables that help control the movement of the character. When user presses 'Key_A' to run left and right after 'Key_D' to run right,
+        character will stop and enter 'Idle' animation, until one of the keys is released.
+        """
+        self.motion_left = False
+        self.motion_right = False
 
     def draw_character(self):
         self.screen.blit(pygame.transform.flip(self.image, self.flip, False), self.img_pos)
@@ -39,52 +43,65 @@ class PlayerCharacter(pygame.sprite.Sprite):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
             if event.type == pygame.KEYDOWN:
                 self.action = 'Running'
                 self.action_divider = 1
                 self.frame_index = 0
 
-                """Y axis"""
-                # if event.key == pygame.K_w:
-                #     self.movement_y[0] = True
-                # if event.key == pygame.K_s:
-                #     self.movement_y[1] = True
                 """X axis"""
                 if event.key == pygame.K_a:
+                    self.action = 'Running'
                     self.movement_x[0] = True
                     self.flip = True
-                    self.direction = -1
+                    self.motion_left = True
 
                 if event.key == pygame.K_d:
+                    self.action = 'Running'
                     self.movement_x[1] = True
                     self.flip = False
-                    self.direction = 1
+                    self.motion_right = True
+
+                if self.motion_right and self.motion_left:
+                    self.action = 'Idle'
+
 
             if event.type == pygame.KEYUP:
                 self.action = 'Idle'
                 self.action_divider = 0
                 self.frame_index = 0
 
-                """Y axis"""
-                # if event.key == pygame.K_w:
-                #     self.movement_y[0] = False
-                # if event.key == pygame.K_s:
-                #     self.movement_y[1] = False
                 """X axis"""
                 if event.key == pygame.K_a:
+                    self.motion_left = False
                     self.movement_x[0] = False
+                    if self.motion_right:
+                        self.flip = False
+                        self.action = 'Running'
+
 
                 if event.key == pygame.K_d:
+                    self.motion_right = False
                     self.movement_x[1] = False
+                    if self.motion_left:
+                        self.flip = True
+                        self.action = 'Running'
+
 
     def update_animation(self):
-        ANIMATION_COOLDOWN = 100
+        ANIMATION_COOLDOWN = 80
 
+        """
+        self.image controls the image generator. We are accessing the list which is returned from animate_character function.
+        We pass 'self.action_divider' that controls the nature of the action (either idle or running) 
+        and 'self.frame_index' which allows the animation to always start from index 0 whenever a new action is introduced.
+        """
         self.image = animate_character(self.action)[self.action_divider][self.frame_index]
         if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
 
+        """Once 'self.frame_index' reaches the final element in the 'animation_list', it starts over from index 0 to maintain fluent animation."""
         if self.frame_index >= len(animate_character(self.action)[self.action_divider]):
             self.frame_index = 0
 
--- Main/player_animation.py
+++ Main/player_animation.py
@@ -2,25 +2,20 @@ import pygame
 import os
 
 current_dir = os.path.dirname(__file__)
-img_path = os.path.join(current_dir,'..','Images','Characters','Player'),
+
 def animate_character(action):
     animation_list = []
     temp_list = []
 
-    # for i in os.listdir(f'../Images/Characters/Player/{action}'):
     for i in os.listdir(os.path.join(current_dir,'..','Images','Characters','Player',action)):
         img = pygame.image.load(os.path.join(current_dir,'..','Images','Characters','Player',action,i))
-        # img = pygame.image.load(f'../Images/Characters/Player/{action}/{i}')
         img = pygame.transform.scale(img, (int(img.get_width() // .7), (int(img.get_height() // .7))))
 
         if action == 'Idle':
-
             temp_list.append(img)
             animation_list.append(temp_list)
 
-
         if action == 'Running':
-
             temp_list.append(img)
             animation_list.append(temp_list)
 
