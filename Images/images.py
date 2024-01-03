d75da1d3e0be38fa44be941e711f4a68094950f4
danielbejcek <daniel.bejcek@seznam.cz>
2024-01-03 10:40:45 +0100

    Collision introduced

--- Images/images.py
+++ Images/images.py
@@ -3,7 +3,7 @@ import os
 current_dir = os.path.dirname(__file__)
 
 img_paths = {
-            "BG_image": os.path.join(current_dir, '..', 'Images', 'bg_main.png'),
+            "BG_image": os.path.join(current_dir, '..', 'Images', 'BG_MAIN.png'),
             "character_img": os.path.join(current_dir, '..', 'Images','Characters','character.png'),
             "char_idle": os.path.join(current_dir,'..','Images','Characters','Player','Idle','0.png'),
             "leaf_particle_1": os.path.join(current_dir,'..', 'Images', 'Particles', 'leaf_particle_1.png')
