345204e50c2a941fbbaedfdb38864593baf5a1bb
danielbejcek <daniel.bejcek@seznam.cz>
2024-03-06 14:05:34 +0100

    Added tests for multiple platform collisions

--- Images/images.py
+++ Images/images.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((1792, 1024))
 
 
 img_paths = {
-            "BG_image": os.path.join(current_dir, '..', 'Images', 'Background','temp_bg2.png'),
+            "BG_image": os.path.join(current_dir, '..', 'Images', 'Background','bg.png'),
             "character_img": os.path.join(current_dir, '..', 'Images','Characters','character.png'),
             "char_idle": os.path.join(current_dir,'..','Images','Characters','Player','Idle','0.png'),
             "arrow_default": os.path.join(current_dir,'..', 'Images', 'Characters','Player','Arrow','Arrow.png'),
@@ -20,12 +20,6 @@ img_paths = {
 
 """Main BG img"""
 bg_img = pygame.image.load(img_paths['BG_image']).convert()
-
-"""Main floor platform"""
-platform_0 = pygame.image.load(img_paths['platform_0'])
-platform_0.get_rect()
-
-platform_1 = pygame.image.load(img_paths['platform_1'])
 
 def draw_background():
     screen.blit(bg_img, (0, 0))
