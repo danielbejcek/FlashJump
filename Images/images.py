e2b46b8c822dc898095a117fbb3f350bc7f114f9
danielbejcek <daniel.bejcek@seznam.cz>
2024-03-07 14:06:05 +0100

    Collision objects added

--- Images/images.py
+++ Images/images.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((1792, 1024))
 
 
 img_paths = {
-            "BG_image": os.path.join(current_dir, '..', 'Images', 'Background','bg.png'),
+            "BG_image": os.path.join(current_dir, '..', 'Images', 'Background','temp_bg2.png'),
             "character_img": os.path.join(current_dir, '..', 'Images','Characters','character.png'),
             "char_idle": os.path.join(current_dir,'..','Images','Characters','Player','Idle','0.png'),
             "arrow_default": os.path.join(current_dir,'..', 'Images', 'Characters','Player','Arrow','Arrow.png'),
--- Main/collisions.py
+++ Main/collisions.py
@@ -4,17 +4,25 @@ def platform_collision():
     platform_list = []
     lamp = pygame.Rect(1002, 748, 185, 2)
     floor = pygame.Rect(0, 900, 1792, 2)
-    roof = pygame.Rect(1348, 626, 200, 2)
+    right_roof = pygame.Rect(1348, 626, 200, 2)
     chimney = pygame.Rect(1660, 462, 100, 2)
-    second_roof = pygame.Rect(1375, 350, 150, 2)
+    second_right_roof = pygame.Rect(1375, 350, 150, 2)
     tent = pygame.Rect(511, 756, 290, 2)
+    sky_platform_right = pygame.Rect(958, 280, 175, 2)
+    sky_platform_left = pygame.Rect(555, 280, 175, 2)
+    sky_platform_bottom = pygame.Rect(555, 450, 175, 2)
+    left_roof = pygame.Rect(145, 620, 290, 2)
 
     platform_list.append(floor)
     platform_list.append(lamp)
-    platform_list.append(roof)
+    platform_list.append(right_roof)
     platform_list.append(chimney)
-    platform_list.append(second_roof)
+    platform_list.append(second_right_roof)
     platform_list.append(tent)
+    platform_list.append(sky_platform_right)
+    platform_list.append(sky_platform_left)
+    platform_list.append(sky_platform_bottom)
+    platform_list.append(left_roof)
 
     return platform_list
 
